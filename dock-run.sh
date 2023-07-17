7d8d577913c472b16a4c53f295bf557999099cf1
Stephane Bourque <stephane.bourque@gmail.com>
2023-07-17 09:43:19 -0700

    Update dock-run.sh
    x

--- dock-run.sh
+++ dock-run.sh
@@ -2,7 +2,7 @@
 
 tag=$(echo ${PWD} | tr / - | cut -b2- | tr A-Z a-z)
 groups=$(id -G | xargs -n1 echo -n " --group-add ")
-params="-v ${PWD}:${PWD} --rm -w ${PWD} -u"$(id -u):$(id -g)" $groups -v/etc/passwd:/etc/passwd:ro -v/etc/group:/etc/group:ro -v$HOME/.gitconfig:$HOME/.gitconfig:ro ${tag}"
+params="-v ${PWD}:${PWD} --rm -w ${PWD} -u"$(id -u):$(id -g)" $groups -v/etc/passwd:/etc/passwd:ro -v/etc/group:/etc/group:ro -v$HOME/.gitconfig:$HOME/.gitconfig:ro ${tag} -v$HOME/.ssh:$HOME/.ssh:rw"
 
 docker build --tag=${tag} docker
 
